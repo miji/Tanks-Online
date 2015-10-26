1d1dbe64ab2c61697686e31a85f345291f533fc1
miji <miguel@dsi.uclm.es>
2015-10-26 14:16:06 +0100

    Players colours

--- Assets/Scripts/Managers/MiniMapManager.cs
+++ Assets/Scripts/Managers/MiniMapManager.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class MiniMapManager : MonoBehaviour {
+public class MiniMapManager : MonoBehaviour
+{
 
 
 	public MObject playerArrow;
@@ -9,7 +11,8 @@ public class MiniMapManager : MonoBehaviour {
 	public MObject playerCircle;
 	public MObject enemyCircle;
 
-	public void UpdateMinimap(){
+	public void UpdateMinimap ()
+	{
 		if (FindObjectOfType<Awareness> ().WhereLocation) {
 			foreach (TankManager tm in GameManager.m_Tanks) {
 				if (tm.m_Health.getCurrentHeath () > 0) {
@@ -19,22 +22,34 @@ public class MiniMapManager : MonoBehaviour {
 						if (tm.m_Instance.GetComponent<MActor> () == null) {
 							//Add player arrow
 							MActor ma = tm.m_Instance.AddComponent<MActor> ();
-							if(FindObjectOfType<Awareness> ().WhereGaze)
+							if (FindObjectOfType<Awareness> ().WhereGaze)
 								ma.objPrefab = playerArrow;
 							else
 								ma.objPrefab = playerCircle;
+
+							//color
+							if (FindObjectOfType<Awareness> ().WhoIdentity)
+								ma.objPrefab.GetComponent<Image> ().color = tm.m_Setup.m_Color;
+
 						}
 					} else {
 						// Is not local
 						if (tm.m_Instance.GetComponent<MActor> () == null) {
 							// Add enemy arrow
 							MActor ma = tm.m_Instance.AddComponent<MActor> ();
-							if(FindObjectOfType<Awareness> ().WhereGaze)
+							if (FindObjectOfType<Awareness> ().WhereGaze)
 								ma.objPrefab = enemyArrow;
 							else
 								ma.objPrefab = enemyCircle;
+
+							//color
+							if (FindObjectOfType<Awareness> ().WhoIdentity)
+								ma.objPrefab.GetComponent<Image> ().color = tm.m_Setup.m_Color;
 						}
 					}
+
+
+
 				} else {
 					// Dead
 					MActor ma = tm.m_Instance.GetComponent<MActor> ();
