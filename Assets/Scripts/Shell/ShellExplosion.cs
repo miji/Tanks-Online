1f0447daa107c1bf72d440e73421c44795f457cf
miji <miguel@dsi.uclm.es>
2015-11-05 16:22:25 +0100

    Several fixes

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -18,17 +18,19 @@ public class ShellExplosion : NetworkBehaviour
 
     private int m_TankMask;                             // A layer mask so that only the tanks are affected by the explosion.
 
-    [ServerCallback]
-    private void Start()
-    {
-        // If it isn't destroyed by then, destroy the shell after it's lifetime.
-        Destroy(gameObject, m_MaxLifeTime);
-
-        // Set the value of the layer mask based solely on the Players layer.
-        m_TankMask = LayerMask.GetMask("Players");
-        GetComponent<Collider>().enabled = false;
-        StartCoroutine(EnableCollision());
-    }
+	private void Start()
+	{
+		if (isServer)
+		{
+			// If it isn't destroyed by then, destroy the shell after it's lifetime.
+			Destroy(gameObject, m_MaxLifeTime);
+			GetComponent<Collider>().enabled = false;
+			StartCoroutine(EnableCollision());
+		}
+		
+		// Set the value of the layer mask based solely on the Players layer.
+		m_TankMask = LayerMask.GetMask("Players");
+	}
 
     //allow to delay a bit the activation of the collider so that it don't collide when spawn close to the canon
     IEnumerator EnableCollision()
@@ -41,43 +43,44 @@ public class ShellExplosion : NetworkBehaviour
     [ServerCallback]
     private void OnTriggerEnter(Collider other)
     {
-        // Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
-        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
-
-        // Go through all the colliders...
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            // ... and find their rigidbody.
-            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
-
-            // If they don't have a rigidbody, go on to the next collider.
-            if (!targetRigidbody)
-                continue;
-
-            // Find the TankHealth script associated with the rigidbody.
-            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
-
-            // If there is no TankHealth script attached to the gameobject, go on to the next collider.
-            if (!targetHealth)
-                continue;
-
-            // Create a vector from the shell to the target.
-            Vector3 explosionToTarget = targetRigidbody.position - transform.position;
-
-            // Calculate the distance from the shell to the target.
-            float explosionDistance = explosionToTarget.magnitude;
-
-            // Calculate the proportion of the maximum distance (the explosionRadius) the target is away.
-            float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
-
-            // Calculate damage as this proportion of the maximum possible damage.
-            float damage = relativeDistance * m_MaxDamage;
+		// Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
+		Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+		
+		// Go through all the colliders...
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			// ... and find their rigidbody.
+			Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+			
+			// If they don't have a rigidbody, go on to the next collider.
+			if (!targetRigidbody)
+				continue;
+			
+			// Find the TankHealth script associated with the rigidbody.
+			TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+			
+			// If there is no TankHealth script attached to the gameobject, go on to the next collider.
+			if (!targetHealth)
+				continue;
+			
+			// Create a vector from the shell to the target.
+			Vector3 explosionToTarget = targetRigidbody.position - transform.position;
+			
+			// Calculate the distance from the shell to the target.
+			float explosionDistance = explosionToTarget.magnitude;
+			
+			// Calculate the proportion of the maximum distance (the explosionRadius) the target is away.
+			float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+			
+			// Calculate damage as this proportion of the maximum possible damage.
+			float damage = relativeDistance * m_MaxDamage;
 
 			// if blue
 			if(strongShot){
-				damage*=1.5f;
+				damage*=1f;
 				//Debug.Log("strong");
 			}else{
+				damage*=0.5f;
 				//Debug.Log("normal");
 			}
 
@@ -87,6 +90,9 @@ public class ShellExplosion : NetworkBehaviour
             // Deal this damage to the tank.
             targetHealth.Damage(damage,attacker,team);
         }
+
+		if (!NetworkClient.active)//if we are ALSO client (so hosting), this will be done by the Destroy so Skip
+			PhysicForces();
 
         // Destroy the shell on clients.
         NetworkServer.Destroy(gameObject);
@@ -105,33 +111,36 @@ public class ShellExplosion : NetworkBehaviour
 
     void ExplodeShell()
     {
-        // Unparent the particles from the shell.
-        m_ExplosionParticles.transform.parent = null;
-
-        // Play the particle system.
-        m_ExplosionParticles.Play();
-
-        // Play the explosion sound effect.
-        m_ExplosionAudio.Play();
-
-        if (isLocalPlayer)
-        {
-            // Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
-            Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
-
-            // Go through all the colliders...
-            for (int i = 0; i < colliders.Length; i++)
-            {
-                // ... and find their rigidbody.
-                Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
-
-                // If they don't have a rigidbody, go on to the next collider.
-                if (!targetRigidbody)
-                    continue;
-
-                // Add an explosion force with no vertical bias.
-                targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
-            }
-        }
+		// Unparent the particles from the shell.
+		m_ExplosionParticles.transform.parent = null;
+		
+		// Play the particle system.
+		m_ExplosionParticles.Play();
+		
+		// Play the explosion sound effect.
+		m_ExplosionAudio.Play();
+		
+		PhysicForces();
     }
+
+	//This apply force on object. Do that on all clients & server as each must apply force to object they own
+	void PhysicForces()
+	{
+		// Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
+		Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+		
+		// Go through all the colliders...
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			// ... and find their rigidbody.
+			Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+			
+			// If they don't have a rigidbody or we don't own that object, go on to the next collider.
+			if (!targetRigidbody || !targetRigidbody.GetComponent<NetworkIdentity>().hasAuthority)
+				continue;
+			
+			// Add an explosion force with no vertical bias.
+			targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+		}
+	}
 }
--- Assets/Scripts/UI/Awareness.cs
+++ Assets/Scripts/UI/Awareness.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Xml;
+using UnityEngine.UI;
 
-public class Awareness : MonoBehaviour {
+public class Awareness : MonoBehaviour
+{
 
 	public string file;
-
+	public Text errorText;
+	public string version;
 	[Header("Present")]
-	public bool WhoIdentity;
+	public bool
+		WhoIdentity;
 	public bool WhoAuthorship; //requires WhatTask
 	public bool WhatTask;
 	public bool WhatStatus; // health + crown + score
@@ -17,95 +21,108 @@ public class Awareness : MonoBehaviour {
 	public bool WhereReach;
 	public bool WherePosition;
 	public bool HowDevice;
-
 	[Header("Past")]
-	public bool WhatTaskHistory; //requires WhatTask
+	public bool
+		WhatTaskHistory; //requires WhatTask
 	public bool WhenEventHistory; //requires WhatTask
 
 	[Header("Future")]
-	public bool WhatNextEvent;
+	public bool
+		WhatNextEvent;
 	public bool WhatNextAbilities;
-
 	[Header("Social & Group Dynamics")]
-	public bool WhoMembers;
+	public bool
+		WhoMembers;
 	public bool WhoOtherMembers;
 	public bool WhatBelonging;
 	public bool WhatGroupGoal;
 
-	void Awake() {
-		DontDestroyOnLoad(transform.gameObject);
+	void Awake ()
+	{
+		DontDestroyOnLoad (transform.gameObject);
 		Load (file);
 	}
 
-	public void ApplyAwarenessElements(){
+	public void ApplyAwarenessElements ()
+	{
 
 		// Disable minimap
-		if(!WhereLocation && !WherePosition) GameObject.Find ("Minimap").gameObject.SetActive (false);
+		if (!WhereLocation && !WherePosition)
+			GameObject.Find ("Minimap").gameObject.SetActive (false);
 
 		//if(!WhatStatus) GameObject.Find ("ScoreText").gameObject.SetActive (false);
 
-		if(!HowDevice) GameObject.Find ("Keys").gameObject.SetActive (false);
+		if (!HowDevice)
+			GameObject.Find ("Keys").gameObject.SetActive (false);
 
-		if(!WhatNextEvent) GameObject.Find ("NextItemText").gameObject.SetActive (false);
+		if (!WhatNextEvent)
+			GameObject.Find ("NextItemText").gameObject.SetActive (false);
 
-		if(!WhatNextAbilities) GameObject.Find ("Items").gameObject.SetActive (false);
+		if (!WhatNextAbilities)
+			GameObject.Find ("Items").gameObject.SetActive (false);
 	}
 
 	void Load (string file)
 	{
 		Debug.Log (file);
-		using (XmlReader reader = XmlReader.Create(file)) {
-			while (reader.Read()) {
-				if (reader.IsStartElement ()) {
-					while (reader.Read()) {
-						if (reader.IsStartElement ()) {
-							switch (reader.Name) {
-							case "Present":
-								WhoIdentity = String2Bool (reader ["WhoIdentity"]);
-								WhoAuthorship = String2Bool (reader ["WhoAuthorship"]);
-								WhatTask = String2Bool (reader ["WhatTask"]);
-								WhatStatus = String2Bool (reader ["WhatStatus"]);
-								WhatAbilities = String2Bool (reader ["WhatAbilities"]);
-								WhereLocation = String2Bool (reader ["WhereLocation"]);
-								WhereGaze = String2Bool (reader ["WhereGaze"]);
-								WhereReach = String2Bool (reader ["WhereReach"]);
-								WherePosition = String2Bool (reader ["WherePosition"]);
-								HowDevice = String2Bool (reader ["HowDevice"]);
-								break;
-							case "Past":
-								WhatTaskHistory = String2Bool (reader ["WhatTaskHistory"]);
-								WhenEventHistory = String2Bool (reader ["WhenEventHistory"]);
-								break;
-							case "Future":
-								WhatNextEvent = String2Bool (reader ["WhatNextEvent"]);
-								WhatNextAbilities = String2Bool (reader ["WhatNextAbilities"]);
-								break;
-							case "SocialGroupDynamics":
-								WhoMembers = String2Bool (reader ["WhoMembers"]);
-								WhoOtherMembers = String2Bool (reader ["WhoOtherMembers"]);
-								WhatBelonging = String2Bool (reader ["WhatBelonging"]);
-								WhatGroupGoal = String2Bool (reader ["WhatGroupGoal"]);
-								break;
+		try {
+			using (XmlReader reader = XmlReader.Create(file)) {
+				while (reader.Read()) {
+					if (reader.IsStartElement ()) {
+						version = reader ["Version"];
+						while (reader.Read()) {
+							if (reader.IsStartElement ()) {
+								switch (reader.Name) {
+								case "Present":
+									WhoIdentity = String2Bool (reader ["WhoIdentity"]);
+									WhoAuthorship = String2Bool (reader ["WhoAuthorship"]);
+									WhatTask = String2Bool (reader ["WhatTask"]);
+									WhatStatus = String2Bool (reader ["WhatStatus"]);
+									WhatAbilities = String2Bool (reader ["WhatAbilities"]);
+									WhereLocation = String2Bool (reader ["WhereLocation"]);
+									WhereGaze = String2Bool (reader ["WhereGaze"]);
+									WhereReach = String2Bool (reader ["WhereReach"]);
+									WherePosition = String2Bool (reader ["WherePosition"]);
+									HowDevice = String2Bool (reader ["HowDevice"]);
+									break;
+								case "Past":
+									WhatTaskHistory = String2Bool (reader ["WhatTaskHistory"]);
+									WhenEventHistory = String2Bool (reader ["WhenEventHistory"]);
+									break;
+								case "Future":
+									WhatNextEvent = String2Bool (reader ["WhatNextEvent"]);
+									WhatNextAbilities = String2Bool (reader ["WhatNextAbilities"]);
+									break;
+								case "SocialGroupDynamics":
+									WhoMembers = String2Bool (reader ["WhoMembers"]);
+									WhoOtherMembers = String2Bool (reader ["WhoOtherMembers"]);
+									WhatBelonging = String2Bool (reader ["WhatBelonging"]);
+									WhatGroupGoal = String2Bool (reader ["WhatGroupGoal"]);
+									break;
+								default:
 
-							
-							default:
-								Debug.LogError("Awareness configuration error");
-								break;
+									break;
+								}
 							}
 						}
-						
 					}
 				}
 			}
+			errorText.text = "Version \"" + version + "\" loaded";
+		} catch (System.IO.FileNotFoundException) {
+			errorText.text = "Awareness configuration file not found";
 		}
 
 	}
 
-	private bool String2Bool(string s)
+	private bool String2Bool (string s)
 	{
-		if (s == "True" || s == "true") return true;
-		else if (s == "False" || s == "false") return false;
-		else return false;
+		if (s == "True" || s == "true" || s == "TRUE" || s == "t" || s == "T" || s == "1")
+			return true;
+		else if (s == "False" || s == "false" || s == "FALSE" || s == "f"|| s == "F"|| s == "0")
+			return false;
+		else
+			return false;
 		
 	}
 }
